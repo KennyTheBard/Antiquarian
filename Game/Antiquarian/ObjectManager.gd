35878553941a4f1adbada95f82ed765e08c032f7
KennyTheBard <octav_teculescu@yahoo.com>
2019-03-22 21:40:16 +0200

    Started implementing Object Manager

---
+++ Game/Antiquarian/ObjectManager.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+# dictionary of all active objects with their ID as key
+var objects = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func add(object):
+	if object is not GameObject:
+		
+	pass
+
+
+func delete(object):
+	
+	pass
