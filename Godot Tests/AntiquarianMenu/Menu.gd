f3cb00943767c3278c667eed97bf5ccb9392c0ce
Kenny <octav_teculescu@yahoo.com>
2019-04-06 18:21:31 +0300

    Imporved menu and some art

--- Godot Tests/AntiquarianMenu/Menu.gd
+++ Godot Tests/AntiquarianMenu/Menu.gd
@@ -4,10 +4,14 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var exit_dialog
+var options_dialog
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	exit_dialog = get_node("Exit/exit_dialog")
 	exit_dialog.hide()
+	
+	options_dialog = get_node("Options/option_dialog")
+	options_dialog.hide()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,4 +21,9 @@ func _ready():
 
 func _on_exit_pressed():
 	exit_dialog.show()
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	options_dialog.show()
 	pass # Replace with function body.
