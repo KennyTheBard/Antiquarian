84c28a8c8576a6296807c9755dbadb5eea9b7496
Kenny <octav_teculescu@yahoo.com>
2019-04-22 22:06:32 +0300

    Compass is working!

--- Game/AntiquarianV2/Scenes/HUDCompass.gd
+++ Game/AntiquarianV2/Scenes/HUDCompass.gd
@@ -1,13 +1,13 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	player = get_parent().get_parent()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	$CompassBack.rect_rotation = player.rot * 180 / PI
+	pass
