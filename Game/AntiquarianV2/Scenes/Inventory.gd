83df5209d2c890dfee8bafe15d81707e9d4f59d7
Kenny <octav_teculescu@yahoo.com>
2019-04-22 21:39:35 +0300

    HUD Inventory now mirrors the Player's inventory

--- Game/AntiquarianV2/Scenes/Inventory.gd
+++ Game/AntiquarianV2/Scenes/Inventory.gd
@@ -1,14 +1,22 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var NumOfItems
+
+var inventory = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	NumOfItems = get_node("NumOfItems").set_text("02845")
-	pass # Replace with function body.
+	inventory = get_parent().get_parent().get_inventory()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+# Update the items displayed in the inventory
+func _process(delta):
+	if inventory:
+		for i in range(len(inventory.slots)):
+			if inventory.slots[i]:
+				var animSprite = inventory.stacks[i][0].get_node("Sprite")
+				$Slots.get_child(i).texture = animSprite.frames.get_frame("default", animSprite.frame)
+			else:
+				$Slots.get_child(i).texture = ImageTexture.new()
+
