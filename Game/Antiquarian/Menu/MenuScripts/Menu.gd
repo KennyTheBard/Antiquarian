e2bf56bb3f793ea8a8e38ccc0510e7bec0fe8b72
KennyTheBard <octav_teculescu@yahoo.com>
2019-05-20 16:00:56 +0300

    Completly integrated the menu

--- Game/Antiquarian/Menu/MenuScripts/Menu.gd
+++ Game/Antiquarian/Menu/MenuScripts/Menu.gd
@@ -1,33 +1,70 @@
 extends Node2D
-var exit_dialog
-var options_dialog
-var menu_dialog
+onready var exit_dialog 	= get_node("Exit/exit_dialog")
+onready var options_dialog 	= get_node("Options/option_dialog")
+onready var menu_dialog 	= get_node("menu_dialog")
+onready var bg 				= get_node("wallpaper")
 
 func _ready():
 	get_node("menu_dialog/title").get("custom_fonts/font").set_size(75)
-
-	menu_dialog = get_node("menu_dialog")
+	
 	menu_dialog.show()
-		
-	exit_dialog = get_node("Exit/exit_dialog")
+	bg.show()
 	exit_dialog.hide()
-	
-	options_dialog = get_node("Options/option_dialog")
 	options_dialog.hide()
-	pass
-	
+
+
 func _on_exit_button_pressed():
-	exit_dialog.show()
 	menu_dialog.hide()
-	pass 
+	exit_dialog.show()
+
 
 func _on_options_button_pressed():
-	options_dialog.show()
 	menu_dialog.hide()
-	pass 
+	options_dialog.show()
 
-func _on_continue_button_pressed():
-	pass 
 
 func _on_new_game_button_pressed():
-	pass 
+	new_game_option()
+
+
+func new_game_option():
+	randomize()
+	var world_seed = randi()
+	save_data(world_seed)
+	start_game(world_seed)
+
+
+func _on_continue_button_pressed():
+	continue_option()
+
+
+func continue_option():
+	var world_seed = load_data()
+	if world_seed == null:
+		new_game_option()
+	start_game(world_seed)
+
+
+func start_game(world_seed):
+	var game_world = load("res://Scenes/GameWorld.tscn").instance()
+	game_world.create_world(world_seed)
+	menu_dialog.hide()
+	bg.hide()
+	get_node("/root").add_child(game_world)
+
+
+func save_data(world_seed):
+	var file = File.new()
+	file.open("user://save_game.dat", File.WRITE)
+	file.store_string(str(world_seed))
+	file.close()
+
+
+func load_data():
+	var file = File.new()
+	if not file.file_exists("user://save_game.dat"):
+		return null
+	file.open("user://save_game.dat", File.READ)
+	var content = file.get_as_text()
+	file.close()
+	return int(content)
--- Game/Antiquarian/Menu/MenuScripts/Video.gd
+++ Game/Antiquarian/Menu/MenuScripts/Video.gd
@@ -1,9 +1,16 @@
 extends Popup
 
 var slider
+var slider_value = 0
 
 func _ready():
 	slider = get_node("video_slider")
-	slider.value = 50
-	pass
-	
+	slider.value = 0
+	slider_value = 50
+
+
+func _process(delta):
+	if (slider_value != slider.value):
+		slider_value = slider.value
+		var alpha = 0.5 * slider_value / 100
+		get_node("../../WhiteScreen").set("modulate",Color(0,0,0,alpha))
