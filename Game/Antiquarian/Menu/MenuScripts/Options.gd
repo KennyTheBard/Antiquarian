60398554c0492f7065e03d65f70ce98a1c1d0dd7
KennyTheBard <octav_teculescu@yahoo.com>
2019-05-20 19:28:04 +0300

    Removed control related menu entries

--- Game/Antiquarian/Menu/MenuScripts/Options.gd
+++ Game/Antiquarian/Menu/MenuScripts/Options.gd
@@ -9,7 +9,6 @@ var options
 func _ready():
 	audio = get_node("audio")
 	video = get_node("video")
-	control = get_node("control")
 	interface = get_node("interface")
 	options = get_node("option_dialog")
 	
@@ -19,10 +18,6 @@ func _on_audio_button_pressed():
 
 func _on_video_button_pressed():
 	video.show()
-	options.hide()
-
-func _on_control_button_pressed():
-	control.show()
 	options.hide()
 
 func _on_interface_button_pressed():
@@ -35,10 +30,6 @@ func _on_option_dialog_back_pressed():
 
 func _on_audio_back_pressed():
 	audio.hide()
-	options.show()
-	
-func _on_control_back_pressed():
-	control.hide()
 	options.show()
 
 func _on_interface_back_pressed():
